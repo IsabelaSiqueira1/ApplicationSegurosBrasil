3c31e92c854806e58e3aa0e40b43debba64a18ba
Isabela Siqueira <isabela.generoso14@gmail.com>
2022-09-29 20:13:10 -0300

    update pagina de login de index e sobre.
    criação da tela de atendimento

--- ApplicationSeguros/Controllers/HomeController.cs
+++ ApplicationSeguros/Controllers/HomeController.cs
@@ -40,6 +40,10 @@ namespace ApplicationSeguros.Controllers
         {
             return View();
         }
+        public IActionResult Atendimento()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
